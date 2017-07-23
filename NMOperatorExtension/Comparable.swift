c74e3cbc2059914c852abd014c9b002a139ccf09
Nicolas Mahé <nicolas@mahe.me>
2017-07-23 16:41:19 +0700

    add classes

---
+++ NMOperatorExtension/Comparable.swift
@@ -0,0 +1,49 @@
+//
+//  Comparable.swift
+//  NMOperatorExtension
+//
+//  Created by Nicolas Mahé on 28/06/17.
+//  Copyright © 2017 Nicolas Mahé. All rights reserved.
+//
+
+import UIKit
+
+public func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l < r
+  case (nil, _?):
+    return true
+  default:
+    return false
+  }
+}
+
+public func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l > r
+  case (nil, _?):
+    return true
+  default:
+    return false
+  }
+}
+
+public func >= <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l >= r
+  default:
+    return !(lhs < rhs)
+  }
+}
+
+public func <= <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l <= r
+  default:
+    return !(lhs > rhs)
+  }
+}
--- NMOperatorExtension/Math.swift
+++ NMOperatorExtension/Math.swift
@@ -8,6 +8,130 @@
 
 import UIKit
 
-class Math: NSObject {
+//----------------------------------------------------------------------------
+// MARK: - Int
+//----------------------------------------------------------------------------
 
+public func +(lhs: Int?, rhs: Int?) -> Int {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l + r
+  case let (l?, nil):
+    return l
+  case let (nil, r?):
+    return r
+  default:
+    return 0
+  }
+}
+
+public func -(lhs: Int?, rhs: Int?) -> Int {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l - r
+  case let (l?, nil):
+    return l
+  case let (nil, r?):
+    return -r
+  default:
+    return 0
+  }
+}
+
+public func *(lhs: Int?, rhs: Int?) -> Int {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l * r
+  default:
+    return 0
+  }
+}
+
+public func /(lhs: Int?, rhs: Int?) -> Int {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l / r
+  default:
+    return 0
+  }
+}
+
+public func +=(lhs: inout Int?, rhs: Int?) {
+  lhs = lhs + rhs
+}
+
+public func -=(lhs: inout Int?, rhs: Int?) {
+  lhs = lhs - rhs
+}
+
+public func *=(lhs: inout Int?, rhs: Int?) {
+  lhs = lhs * rhs
+}
+
+public func /=(lhs: inout Int?, rhs: Int?) {
+  lhs = lhs / rhs
+}
+
+//----------------------------------------------------------------------------
+// MARK: - Float
+//----------------------------------------------------------------------------
+
+public func +(lhs: Float?, rhs: Float?) -> Float {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l + r
+  case let (l?, nil):
+    return l
+  case let (nil, r?):
+    return r
+  default:
+    return 0
+  }
+}
+
+public func -(lhs: Float?, rhs: Float?) -> Float {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l + r
+  case let (l?, nil):
+    return l
+  case let (nil, r?):
+    return -r
+  default:
+    return 0
+  }
+}
+
+public func *(lhs: Float?, rhs: Float?) -> Float {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l * r
+  default:
+    return 0
+  }
+}
+
+public func /(lhs: Float?, rhs: Float?) -> Float {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l / r
+  default:
+    return 0
+  }
+}
+
+public func +=(lhs: inout Float?, rhs: Float?) {
+  lhs = lhs + rhs
+}
+
+public func -=(lhs: inout Float?, rhs: Float?) {
+  lhs = lhs - rhs
+}
+
+public func *=(lhs: inout Float?, rhs: Float?) {
+  lhs = lhs * rhs
+}
+
+public func /=(lhs: inout Float?, rhs: Float?) {
+  lhs = lhs / rhs
 }
